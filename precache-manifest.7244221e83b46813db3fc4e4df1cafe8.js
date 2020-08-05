8bcac53df106ce726b596ce7373e13b165e96151
Tatiana <eyesdwn@gmail.com>
2020-08-05 14:31:40 +0300

    Updates

---
+++ precache-manifest.7244221e83b46813db3fc4e4df1cafe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed46350485d081ff80ee4a4bdf50708",
+    "url": "/rickandmorty/index.html"
+  },
+  {
+    "revision": "57b8fa26b8cbd03c08b7",
+    "url": "/rickandmorty/static/css/main.7120d15a.chunk.css"
+  },
+  {
+    "revision": "b614bdb7041e6f6836ec",
+    "url": "/rickandmorty/static/js/2.c2496db0.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/rickandmorty/static/js/2.c2496db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b8fa26b8cbd03c08b7",
+    "url": "/rickandmorty/static/js/main.01605664.chunk.js"
+  },
+  {
+    "revision": "22cff0bd1be6aba1cb5d",
+    "url": "/rickandmorty/static/js/runtime-main.debbbf36.js"
+  }
+]);
